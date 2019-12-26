4f8594a32b16ed247f24ca08ef54bffc11f068b2
Dave Quah <quahdave@gmail.com>
2019-12-26 22:58:11 +0800

    [dave] add dummy identity cards

---
+++ src/components/IdentityCardBack/IdentityCardBack.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+const IdentityCardBack: FC<{}> = () => {
+  return <div className="">asdasd</div>;
+};
+
+export default IdentityCardBack;
---
+++ src/components/IdentityCardFront/IdentityCardFront.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+const IdentityCardFront: FC<{}> = () => {
+  return <div className="">asdasd</div>;
+};
+
+export default IdentityCardFront;
