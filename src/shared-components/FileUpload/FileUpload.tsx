cf7f83f17407902e282389ec07940eef353d3198
Dave Quah <quahdave@gmail.com>
2019-12-30 22:02:12 +0800

    [dave] add file upload component

---
+++ src/shared-components/FileUpload/FileUpload.tsx
@@ -0,0 +1,41 @@
+import React, { FC, ChangeEventHandler, useRef, ReactNode } from "react";
+
+import Button, { ButtonAppearance } from "../Button";
+
+interface Props {
+  id: string;
+  children: ReactNode;
+  onChange: ChangeEventHandler;
+}
+
+const FileUpload: FC<Props> = ({ id, children, onChange }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleClick = () => {
+    const { current } = inputRef;
+
+    if (current) {
+      current.click();
+    }
+  };
+
+  return (
+    <>
+      <input
+        ref={inputRef}
+        type="file"
+        className="hidden"
+        onChange={onChange}
+      />
+      <Button
+        id={id}
+        appearance={ButtonAppearance.TERTIARY}
+        onClick={handleClick}
+      >
+        {children}
+      </Button>
+    </>
+  );
+};
+
+export default FileUpload;
