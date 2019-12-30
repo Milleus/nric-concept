1a899660b0fac85342f9ca1e7892d49f0aea3819
Dave Quah <quahdave@gmail.com>
2019-12-30 22:01:11 +0800

    [dave] add button component

---
+++ src/shared-components/Button/Button.tsx
@@ -0,0 +1,49 @@
+import classnames from "classnames";
+import React, { FC, ReactNode, MouseEventHandler } from "react";
+
+export enum ButtonAppearance {
+  PRIMARY = "primary",
+  SECONDARY = "secondary",
+  TERTIARY = "tertiary"
+}
+
+interface Props {
+  id: string;
+  appearance: ButtonAppearance;
+  children: ReactNode;
+  onClick?: MouseEventHandler;
+  type?: "submit" | "reset" | "button";
+}
+
+const Button: FC<Props> = ({
+  id,
+  appearance,
+  children,
+  type = "submit",
+  onClick
+}) => {
+  const buttonConditionalClass = {
+    "bg-blue-700 text-white border-blue-700":
+      appearance === ButtonAppearance.PRIMARY,
+    "bg-blue-200 text-blue-700 border-blue-200":
+      appearance === ButtonAppearance.SECONDARY,
+    "bg-white text-blue-700 border-blue-700":
+      appearance === ButtonAppearance.TERTIARY
+  };
+
+  return (
+    <button
+      id={id}
+      type={type}
+      className={classnames(
+        `font-semibold py-2 px-4 border-2 rounded focus:outline-none hover:shadow-md`,
+        buttonConditionalClass
+      )}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
