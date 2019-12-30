c353a15cae9e5e5654ff0c73f128c02b4758c3a8
Dave Quah <quahdave@gmail.com>
2019-12-30 17:16:37 +0800

    [dave] remove nric front and back dummy components

--- src/components/IdentityCardBack/IdentityCardBack.tsx
+++
@@ -1,7 +0,0 @@
-import React, { FC } from "react";
-
-const IdentityCardBack: FC<{}> = () => {
-  return <div className="">asdasd</div>;
-};
-
-export default IdentityCardBack;
--- src/components/IdentityCardFront/IdentityCardFront.tsx
+++
@@ -1,7 +0,0 @@
-import React, { FC } from "react";
-
-const IdentityCardFront: FC<{}> = () => {
-  return <div className="">asdasd</div>;
-};
-
-export default IdentityCardFront;
